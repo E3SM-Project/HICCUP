f57eb968e90c6fb1de3a9c605966c2888ceba423
whannah1 <whannah1@gmail.com>
2020-01-24 12:00:18 -0700

    Updated script for retreiving ERA5 data

--- get_ERA5_data.py
+++ get_ERA5_data.py
@@ -3,33 +3,76 @@ import os
 import cdsapi
 server = cdsapi.Client()
 
-yr1,mn1,dy1 = 2018,1,1
-yr2,mn2,dy2 = 2018,1,2
-date_range = f'{yr1}{mn1}{dy1}/to/{yr2}{mn2}{dy2}'
+get_atm = True
+get_sfc = True
+get_lnd = True
 
-var = ['Ps','Ts','U','T','Q']
+yr_list = ['2018']
+mn_list = ['01']
+dy_list = ['01']
 
-lev = ['50','70','100','125','150','175','200'
-      ,'225','250','300','350','400','450','500'
-      ,'550','600','650','700','750','775','800'
-      ,'825','850','875','900','925','950','975','1000'],
+# time_list = ['00:00','03:00','06:00','09:00','12:00','15:00','18:00','21:00']
+time_list = ['00:00']
 
-output_path = os.getenv('HOME')+'/Data/Obs/ERA5/raw_netcdf/'
-output_file = output_path+'ERA5.HICCUP_TEST.nc'
+lev = [ '50', '70','100','125','150','175','200','225','250','300','350'
+      ,'400','450','500','550','600','650','700','750','775','800','825'
+      ,'850','875','900','925','950','975','1000']
 
-server.retrieve('reanalysis-era5-complete',{
-    'product_type'  : 'reanalysis',
-    'levtype'       : 'pl', 
-    'levelist'      : lev,
-    'time'          : ['00:00','03:00','06:00','09:00','12:00','15:00','18:00','21:00'],
-    'date'          : date_range,
-    'format'        : 'netcdf',
-    'variable'      : ['temperature','specific_humidity','geopotential'
-                      ,'u_component_of_wind','v_component_of_wind'
-                      ,'surface_pressure','skin_temperature'
-                      ,'sea_surface_temperature'
-                      ],
-}, output_file)
+output_path = os.getenv('PWD')
 
+output_file_plv = output_path+'ERA5.HICCUP_TEST.atm.nc'
+output_file_sfc = output_file_plv.replace('atm.nc','sfc.nc')
+output_file_lnd = output_file_plv.replace('atm.nc','lnd.nc')
 
-# ,'mean_sea_level_pressure'  
+#-------------------------------------------------------------------------------
+# atmossphere pressure level data
+if get_atm:
+    server.retrieve('reanalysis-era5-pressure-levels',{
+        'product_type'  : 'reanalysis',
+        'pressure_level': lev,
+        'time'          : time_list,
+        'day'           : dy_list,
+        'month'         : mn_list,
+        'year'          : yr_list,
+        'format'        : 'netcdf',
+        'variable'      : ['temperature'
+                          ,'specific_humidity'
+                          ,'geopotential'
+                          ,'u_component_of_wind'
+                          ,'v_component_of_wind'
+                          ],
+    }, output_file_plv)
+#-------------------------------------------------------------------------------
+# atmosphere surface data
+if get_sfc:
+    server.retrieve('reanalysis-era5-single-levels',{
+        'product_type'  : 'reanalysis',
+        'time'          : time_list,
+        'day'           : dy_list,
+        'month'         : mn_list,
+        'year'          : yr_list,
+        'format'        : 'netcdf',
+        'variable'      : ['surface_pressure'
+                          ,'skin_temperature'
+                          ,'sea_surface_temperature'
+                          ],
+    }, output_file_sfc)
+#-------------------------------------------------------------------------------
+# land model data
+if get_lnd:
+    server.retrieve('reanalysis-era5-land',{
+            'format': 'netcdf',
+            'variable': [
+                'leaf_area_index_high_vegetation', 'leaf_area_index_low_vegetation', 'skin_reservoir_content',
+                'skin_temperature', 'snow_albedo', 'snow_cover',
+                'snow_density', 'snow_depth', 'snow_depth_water_equivalent',
+                'soil_temperature_level_1', 'soil_temperature_level_2', 'soil_temperature_level_3',
+                'soil_temperature_level_4', 'temperature_of_snow_layer', 'volumetric_soil_water_layer_1',
+                'volumetric_soil_water_layer_2', 'volumetric_soil_water_layer_3', 'volumetric_soil_water_layer_4',
+            ],
+            'time'          : time_list,
+            'day'           : dy_list,
+            'month'         : mn_list,
+            'year'          : yr_list,
+    },output_file_lnd)
+#-------------------------------------------------------------------------------
