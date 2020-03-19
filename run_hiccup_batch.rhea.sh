c1f000cbef2f7d0ab2f5a511a6d0bfecc214e6df
Walter Hannah <whannah1@gmail.com>
2020-03-19 11:26:15 -0400

    Add batch script for Rhea (OLCF)
    	new file:   run_hiccup_batch.rhea.sh

---
+++ run_hiccup_batch.rhea.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --account=CLI115
+#SBATCH --time=8:00:00
+#SBATCH --partition=batch
+#SBATCH --nodes=1
+#SBATCH --output=slurm-%x-%j.out
+###SBATCH --mail-user=hannah6@llnl.gov
+###SBATCH --mail-type=END,FAIL
+
+# To run this batch script, use the command below to set the output grid from the command line:
+# NE=1024 ; sbatch --job-name=hiccup_ne$NE --export=NE=$NE ./run_hiccup_batch.rhea.sh
+
+source activate hiccup_env
+
+# Set NE if not set on the command line
+if [ -z ${NE+x} ]; then NE=30; fi
+
+time python -u ./create_initial_condition.py --hgrid=ne${NE}np4
+
+# Notes:
+# "time" is added to add the execution time to the log file.
+# the "-u" option allows the log file to update in real time 
+# which is useful for montioring the batch job.
