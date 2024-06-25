a0fc0d7c02d9db107b2d8ae45b6d5bb8e2999d75
Walter Hannah <hannah6@llnl.gov>
2024-06-25 12:50:15 -0700

    update get_nudging_data.ERA5.py

--- get_nudging_data.ERA5.py
+++ get_nudging_data.ERA5.py
@@ -1,58 +1,92 @@
+#!/usr/bin/env python
+#---------------------------------------------------------------------------------------------------
+# links to CDS web interface:
+#   https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-era5-pressure-levels
+#   https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-era5-single-levels
+# A list of available variables can also be found in the ERA5 documentation:
+#   https://confluence.ecmwf.int/display/CKB/ERA5%3A+data+documentation
+#---------------------------------------------------------------------------------------------------
 import os, cdsapi, datetime, pandas as pd
-from datetime import date, timedelta
 server = cdsapi.Client()
+#---------------------------------------------------------------------------------------------------
+class clr:END,RED,GREEN,MAGENTA,CYAN = '\033[0m','\033[31m','\033[32m','\033[35m','\033[36m'
+#---------------------------------------------------------------------------------------------------
+usage = f'''
+This script is intended to streamline the aquisition of ERA5 pressure level 
+and surface data for processing data for E3SM atmospheric nudging with HICCUP
+
+Minimum example of requesting single data file for single initialization date/time:
+  {clr.GREEN}python get_nudging_data.ERA5.py --start-date=<yyyymmdd>{clr.END}
+
+Example of requesting multiple data files spanning a range of dates/times every 3-hours:
+  {clr.GREEN}python get_nudging_data.ERA5.py --start-date=<yyyymmdd> --final-date=<yyyymmdd> --start-hour=<hh> --final-hour=<hh> --data-freq=3h --output-root=<path>{clr.END}
+'''
+from optparse import OptionParser
+parser = OptionParser(usage=usage)
+parser.add_option('--start-date',  dest='start_date',  default=None,  help='date of first file [yyyymmdd]')
+parser.add_option('--final-date',  dest='final_date',  default=None,  help='date of last file [yyyymmdd] (optional)')
+parser.add_option('--start-hour',  dest='start_hour',  default='00',  help='UTC hour of first file (default=00Z)')
+parser.add_option('--final-hour',  dest='final_hour',  default='00',  help='UTC hour of last file (default=00Z)')
+parser.add_option('--data-freq',   dest='data_freq',   default='24h', help='frequency of data files (default=24h)')
+parser.add_option('--output-root', dest='output_root', default='./',  help='Output path for data files (default is PWD)')
+(opts, args) = parser.parse_args()
+#---------------------------------------------------------------------------------------------------
+# check that input arguments are valid
+if opts.start_date is None: raise ValueError(f'{clr.RED}initialization date was not specified{clr.END}')
+if opts.final_date is None: opts.final_date = opts.start_date
+#---------------------------------------------------------------------------------------------------
+# build list of dates and times from input arguments
+beg_date = datetime.datetime.strptime(f'{opts.start_date} {opts.start_hour}', '%Y%m%d %H')
+end_date = datetime.datetime.strptime(f'{opts.final_date} {opts.final_hour}', '%Y%m%d %H')
+datetime_list = pd.date_range(beg_date, end_date, freq=opts.data_freq)
+#---------------------------------------------------------------------------------------------------
+# Request all available pressure levels
+lev = [  '1',  '2',  '3',  '5',  '7', '10', '20', '30', '50', '70','100','125',
+       '150','175','200','225','250','300','350','400','450','500','550','600',
+       '650','700','750','775','800','825','850','875','900','925','950','975','1000']
+#---------------------------------------------------------------------------------------------------
+# loop over dates
 
 get_atm = True
 get_sfc = True
 
-bdate_str,edate_str = '2020-01-20 00','2020-01-26 21' # 2024 SCREAM autocalibration (DYAMOND2)
-
-# Create list of date and time 
-beg_date = datetime.datetime.strptime(bdate_str, '%Y-%m-%d %H')
-end_date = datetime.datetime.strptime(edate_str, '%Y-%m-%d %H')
-datetime_list = pd.date_range(beg_date, end_date, freq='3H')
-
-# All available pressure levels
-lev = [  '1',  '2',  '3',  '5',  '7', '10', '20', '30', '50', '70','100','125','150','175','200'
-      ,'225','250','300','350','400','450','500','550','600','650','700','750','775','800','825'
-      ,'850','875','900','925','950','975','1000']
-
-output_path = os.getenv('HOME')+'/HICCUP/data_scratch/nudge_data'
-
-os.makedirs(output_path, exist_ok=True)
-
-# ------------------------------------------------------------------------------
 for t in datetime_list:
-   yr = t.strftime("%Y")
-   mn = t.strftime("%m")
-   dy = t.strftime("%d")
-   hr = t.strftime("%H")
-   # ---------------------------------------------------------------------------
-   output_file_plv = f'{output_path}/ERA5.atm.{yr}-{mn}-{dy}-{hr}:00.nc'
-   output_file_sfc = f'{output_path}/ERA5.sfc.{yr}-{mn}-{dy}-{hr}:00.nc'
-   # ---------------------------------------------------------------------------
-   # atmosphere pressure level data
-   if get_atm:
+  #-----------------------------------------------------------------------------
+  # parse date/time information
+  yr = t.strftime("%Y")
+  mn = t.strftime("%m")
+  dy = t.strftime("%d")
+  hr = t.strftime("%H")
+  hr_min = f'{hr}:00'
+  #-----------------------------------------------------------------------------
+  # Specify output file names
+  output_file_plv = f'{opts.output_root}/ERA5.atm.{yr}-{mn}-{dy}.{hr}.nc'
+  output_file_mlv = output_file_plv.replace('.atm.','.mlev.')
+  output_file_sfc = output_file_plv.replace('.atm.','.sfc.')
+  output_file_lnd = output_file_plv.replace('.atm.','.lnd.')
+  #-----------------------------------------------------------------------------
+  # atmossphere pressure level data
+  if get_atm:
       server.retrieve('reanalysis-era5-pressure-levels',{
-         'product_type'  : 'reanalysis',
-         'pressure_level': lev,
-         'time'          : f'{hr}:00',
-         'day'           : dy,
-         'month'         : mn,
-         'year'          : yr,
-         'format'        : 'netcdf',
-         'variable'      : ['u_component_of_wind', 'v_component_of_wind']
+        'product_type'  : 'reanalysis',
+        'format'        : 'netcdf',
+        'pressure_level': lev,
+        'time'          : hr_min,
+        'day'           : dy,
+        'month'         : mn,
+        'year'          : yr,
+        'variable'      : ['u_component_of_wind', 'v_component_of_wind']
       }, output_file_plv)
-   # ---------------------------------------------------------------------------
-   # surface data
-   if get_sfc:
+  #-----------------------------------------------------------------------------
+  # surface data
+  if get_sfc:
       server.retrieve('reanalysis-era5-single-levels',{
-         'product_type'  : 'reanalysis',
-         'time'          : f'{hr}:00',
-         'day'           : dy,
-         'month'         : mn,
-         'year'          : yr,
-         'format'        : 'netcdf',
-         'variable'      : ['surface_pressure']
+        'product_type'  : 'reanalysis',
+        'format'        : 'netcdf',
+        'time'          : hr_min,
+        'day'           : dy,
+        'month'         : mn,
+        'year'          : yr,
+        'variable'      : ['surface_pressure']
       }, output_file_sfc)
-   # ----------------------------------------------------------------------------
+  #-----------------------------------------------------------------------------
