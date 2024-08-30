aa8fc48f070621c1ed9cf221ec27404b9d14fe1d
Walter Hannah <hannah6@llnl.gov>
2024-08-30 09:01:50 -0700

    clean up hiccup utilities and add print_mem_usage

--- hiccup/hiccup_utilities.py
+++ hiccup/hiccup_utilities.py
@@ -1,14 +1,12 @@
 # This module contains some useful utility routines used in hiccup_data_class.py
 import os, sys, re, shutil, subprocess as sp
-# from time import perf_counter
-# timer_msg_all = []
+import resource, psutil
 # ------------------------------------------------------------------------------
 # Global verbosity default
 hiccup_verbose = False
 verbose_indent = ''
 # ------------------------------------------------------------------------------
 # class for terminal colors
-# ------------------------------------------------------------------------------
 class tcolor:
     """ simple class for coloring terminal text """
     ENDC, BLACK, RED     = '\033[0m','\033[30m','\033[31m'
@@ -16,7 +14,6 @@ class tcolor:
     MAGENTA, CYAN, WHITE = '\033[35m','\033[36m','\033[37m'
 # ------------------------------------------------------------------------------
 # Common method for printing and running commands
-# ------------------------------------------------------------------------------
 def run_cmd(cmd,verbose=None,prepend_line=True,use_color=True,shell=False,prefix='  ',suffix=''):
     """
     Method to encapsulate running system commands and checking for failures
@@ -33,7 +30,6 @@ def run_cmd(cmd,verbose=None,prepend_line=True,use_color=True,shell=False,prefix
     return
 # ------------------------------------------------------------------------------
 # Method for checking if required software is installed
-# ------------------------------------------------------------------------------
 def check_dependency(cmd):
     """ 
     Check for required system commands 
@@ -42,7 +38,6 @@ def check_dependency(cmd):
     return
 # ------------------------------------------------------------------------------
 # Methods for parsing and comparing version strings for required software
-# ------------------------------------------------------------------------------
 def suffix_as_tuple(suffix):
     """
     """
@@ -71,7 +66,6 @@ def compare_version(version, required_version='4.9.2-alpha'):
     return (v0 > v1) or ((v0 == v1) and suffix0 >= suffix1)
 # ------------------------------------------------------------------------------
 # Check version of NCO - and fail if not recent enough
-# ------------------------------------------------------------------------------
 def check_nco_version():
     """
     NCO needs to include a vertical interpolation bug fix added in 4.9.2-alpha09
@@ -92,45 +86,12 @@ def check_nco_version():
         err_msg += f'\nHICCUP requires NCO version {min_version} or higher'
         raise EnvironmentError(err_msg)
     return
-# # ------------------------------------------------------------------------------
-# # Print individual timer information
-# # ------------------------------------------------------------------------------
-# def print_timer(timer_start,use_color=True,prefix='\n',caller=None,print_msg=True):
-#     """
-#     Print the final timer result based on input start time
-#     Also update timer_msg_all for use in print_timer_summary
-#     """
-#     # if caller is not provider get name of parent routine
-#     if caller is None: caller = sys._getframe(1).f_code.co_name
-#     # calculate elapsed time
-#     etime = perf_counter()-timer_start
-#     time_str = f'{etime:10.1f} sec'
-#     # add minutes if longer than 60 sec or 2 hours
-#     if etime>60       : time_str += f' ({(etime/60):4.1f} min)'
-#     # if etime>(2*3600) : time_str += f' ({(etime/3600):.1f} hr)'
-#     # create the timer result message
-#     msg = f'{caller:40} elapsed time: {time_str}'
-#     # add message to list of messages for print_timer_summary
-#     timer_msg_all.append(msg)
-#     # Apply color
-#     if use_color : msg = tcolor.YELLOW + msg + tcolor.ENDC
-#     # print the message
-#     print(prefix+msg)
-#     return
-# # ------------------------------------------------------------------------------
-# # Print a summary of timer information
-# # ------------------------------------------------------------------------------
-# def print_timer_summary():
-#     """
-#     Print timer summary based on information compiled by print_timer()
-#     """
-#     # Add timer info for all if timer_start_total was set
-#     if timer_start_total is not None: 
-#         print_timer(timer_start_total,caller=f'Total',print_msg=False)
-#     if do_timers:
-#         print(verbose_indent+'\nHICCUP Timer results:')
-#         for msg in timer_msg_all:
-#             print(verbose_indent+f'  {msg}')
-#     return
 # ------------------------------------------------------------------------------
+# Method for monitoring memory usage (for debugging and development)
+def print_mem_usage(indent='  ',msg=''):
+    # mem_rss_GB = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss * 1e-6
+    mem_rss_GB = psutil.Process().memory_info().rss * 1e-9
+    mem_vms_GB = psutil.Process().memory_info().vms * 1e-9
+    print(verbose_indent+indent+f'Memory: {mem_rss_GB:8.2f} / {mem_vms_GB:8.2f}  GB  (RSS/VMS)  {msg}')
+    return
 # ------------------------------------------------------------------------------
